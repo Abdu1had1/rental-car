0417036375877385f7d5914d2edd5b33bcd4ae92
abdu1had1 <abdulhadi997kips@gmail.com>
2024-02-05 16:57:03 +0500

    new changes

--- src/main.ts
+++ src/main.ts
@@ -5,42 +5,58 @@ import { AuthGuard } from './auth/auth.guard';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { INestApplication } from '@nestjs/common';
 import { configure as serverlessExpress } from '@vendia/serverless-express';
+import path from 'path';
+import express from 'express';
 
 let server: Handler;
 
+
+// Function to setup Swagger UI
+function setupSwagger(nestApp: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('Car Rental')
+    .setDescription('The car rental API description')
+    .setVersion('0.0.1')
+    .addServer('/dev/api')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT Token',
+      in: 'header',
+    }, 'JWT-auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(nestApp, options);
+  SwaggerModule.setup('/swagger', nestApp, document,{
+    customSiteTitle: 'Samlple',
+    swaggerOptions:{
+      docExpansion: 'none',
+      operationSorter: 'alpha',
+      tagSorter: 'alpha',
+    },
+  });
+}
+
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  app.use(express.static(path.join(__dirname, '..', 'public')));
   app.enableCors({
     origin: '*', // You might want to specify the actual origin(s) here
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
   });
+
   setupSwagger(app);
   await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
 
-function setupSwagger(nestApp: INestApplication): void {
-  const options = new DocumentBuilder()
-    .setTitle('Car Rental')
-    .setDescription('The car rental API description')
-    .setVersion('1.0')
-    .addBearerAuth({
-      type: "http",
-      scheme: "bearer",
-      bearerFormat: "JWT",
-      name: "JWT",
-      description: "Enter JWT Token",
-      in: "header"
-    }, "JWT-auth")
-    .build();
 
-  const document = SwaggerModule.createDocument(nestApp, options);
-  SwaggerModule.setup('api', nestApp, document);
-}
-
+// Lambda handler function
 export const handler: Handler = async (
   event: any,
   context: Context,
